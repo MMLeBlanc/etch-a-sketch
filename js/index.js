74b320260568ac581d2fc3ecd1656d2c615392bd
MMLeBlanc <matthewmleblanc@gmail.com>
2022-03-22 22:20:02 -0500

    Finished the logic for the game and added a reset button

--- js/index.js
+++ js/index.js
@@ -1,19 +1,43 @@
-function createBoard() {
-    for (let i = 0; i < 4; i++) {
+function createBoard(x) {
+    for (let i = 0; i < x; i++) {
         const row = document.createElement('div');
         row.classList.add(`row${i}`, 'row');
         const container = document.querySelector('.board');
         container.appendChild(row);
 
-        for (let j = 0; j < 4; j++) {
+        for (let j = 0; j < x; j++) {
             const section = document.createElement('div');
             section.classList.add('section');
 
             const container = document.querySelector(`.row${i}`);
             container.appendChild(section);
         }
-
     }
 }
 
-createBoard();
+function addHover() {
+    const squareHover = document.querySelectorAll('.section');
+    squareHover.forEach(square => {
+        square.addEventListener('mouseover', e => {
+            e.target.style.backgroundColor = 'lightgrey';
+        });
+    });
+}
+
+createBoard(4, 4);
+addHover();
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.section');
+    squares.forEach(e => {
+        e.style.backgroundColor = 'white';
+    });
+    let response = window.prompt("Please enter number of squares per side:");
+    const board = document.querySelector('.board');
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
+    createBoard(Number(response));
+    addHover();
+});
